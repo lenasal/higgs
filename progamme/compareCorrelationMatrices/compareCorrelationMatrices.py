1e7973214bfb2db747413bdb6d8ddba4957a3d9e
denido <uxdcy@fphct13.physik.uni-karlsruhe.de>
2016-07-05 18:38:59 +0200

    extracted rankings all variables

--- progamme/compareCorrelationMatrices/compareCorrelationMatrices.py
+++ progamme/compareCorrelationMatrices/compareCorrelationMatrices.py
@@ -9,17 +9,17 @@ from ROOT import Double
 
 
 filename="../00daten/trainingsOutput/001_default/TMVAout.root"
-correlation_boundary=15
+correlation_boundary=1
 
 
-variable_names= [
-      "DER_mass_MMC", "DER_mass_transverse_met_lep", "DER_mass_vis", "DER_pt_h",
-      "DER_deltaeta_jet_jet", "DER_mass_jet_jet", "DER_prodeta_jet_jet",
-      "DER_deltar_tau_lep", "DER_pt_tot", "DER_sum_pt", "DER_pt_ratio_lep_tau",
-      "DER_met_phi_centrality", "DER_lep_eta_centrality", "PRI_tau_pt", "PRI_tau_eta", "PRI_tau_phi",
-      "PRI_lep_pt", "PRI_lep_eta", "PRI_lep_phi", "PRI_met", "PRI_met_phi", "PRI_met_sumet",
-      "PRI_jet_num", "PRI_jet_leading_pt", "PRI_jet_leading_eta", "PRI_jet_leading_phi",
-      "PRI_jet_subleading_pt","PRI_jet_subleading_eta","PRI_jet_subleading_phi","PRI_jet_all_pt"
+
+variable_names= ["DER_mass_MMC", "DER_mass_transverse_met_lep", "DER_mass_vis", "DER_pt_h",
+            "DER_deltaeta_jet_jet", "DER_mass_jet_jet", "DER_prodeta_jet_jet",
+            "DER_deltar_tau_lep", "DER_pt_tot", "DER_sum_pt", "DER_pt_ratio_lep_tau",
+            "DER_met_phi_centrality", "DER_lep_eta_centrality", "PRI_tau_pt", "PRI_tau_eta", "PRI_tau_phi",
+            "PRI_lep_pt", "PRI_lep_eta", "PRI_lep_phi", "PRI_met", "PRI_met_phi", "PRI_met_sumet",
+            "PRI_jet_num", "PRI_jet_leading_pt", "PRI_jet_leading_eta", "PRI_jet_leading_phi",
+            "PRI_jet_subleading_pt","PRI_jet_subleading_eta","PRI_jet_subleading_phi","PRI_jet_all_pt"
 ]
 
 
@@ -48,3 +48,6 @@ for x in range(xN):
   if can_be_ignored:
     variable_name_x = variable_names[x]
     print(variable_name_x, "can be ignored")
+    
+    
+ 
